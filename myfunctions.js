9fd3a6b169b9aba8a5ff5eaf00b9e963fce3f618
tejash <tejashyadavinfo@gmail.com>
2025-01-20 19:45:25 +0530

    add function concept

--- myfunctions.js
+++ myfunctions.js
@@ -1,26 +1,39 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var add = function (a, b) {
-  return a + b;
-};
-var getUpper = function (a) {
-  return a.toLocaleUpperCase();
+var add = function (a, b) { return a + b; };
+var addTwo = function (a) { return a + 2; };
+var getUpper = function (a) { return a.toLocaleUpperCase(); };
+var getValue = function (myVal) {
+    if (myVal > 5) {
+        return true;
+    }
+    return "200 ok";
 };
 var signUp = function (name, email, isPaid) {
-  if (isPaid === void 0) {
-    isPaid = false;
-  }
-  return {
-    name: name,
-    email: email,
-    isPaid: isPaid,
-  };
+    if (isPaid === void 0) { isPaid = false; }
+    return {
+        name: name,
+        email: email,
+        isPaid: isPaid,
+    };
 };
 var signIn = function (email) {
-  return {
-    email: email,
-  };
+    return {
+        email: email,
+    };
 };
+var getHello = function (message) {
+    return message;
+};
+var heros = ["Superman", "Batman", "Wonder Woman"];
+heros.map(function (item) {
+    return "hero name is ".concat(item);
+});
+var myvalues = addTwo(3);
+console.log(heros);
+console.log(getHello("Good Morninig Dears"));
+console.log(myvalues);
+console.log(getValue(4));
 console.log(add(4, 5));
 console.log(getUpper("Hello Worlds"));
 console.log(signUp("payal", "pty22112000@gmail.com"));
--- myfunctions.ts
+++ myfunctions.ts
@@ -1,6 +1,15 @@
 const add = (a: number, b: number) => a + b;
 
+const addTwo = (a: number): number => a + 2;
+
 const getUpper = (a: string) => a.toLocaleUpperCase();
+
+const getValue = (myVal: number): boolean | string => {
+  if (myVal > 5) {
+    return true;
+  }
+  return "200 ok";
+};
 
 const signUp = (name: string, email: string, isPaid: boolean = false) => {
   return {
@@ -16,8 +25,25 @@ const signIn = (email: string) => {
   };
 };
 
+const getHello = (message: string): string => {
+  return message;
+};
+
+const heros: Array<string> = ["Superman", "Batman", "Wonder Woman"];
+
+heros.map((item: string): string => {
+  return `hero name is ${item}`;
+});
+
+const myvalues = addTwo(3);
+
+console.log(heros);
+console.log(getHello("Good Morninig Dears"));
+console.log(myvalues);
+console.log(getValue(4));
 console.log(add(4, 5));
 console.log(getUpper("Hello Worlds"));
 console.log(signUp("payal", "pty22112000@gmail.com"));
 console.log(signIn("pty22112000@gmail.com"));
+
 export {};
